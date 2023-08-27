e57fe0fad121632b7f8ff2eb6a019a6e8fae50eb
Guillermo <guillermo.maldonado@ucb.edu.bo>
2023-08-27 01:23:57 -0400

    saludador identificador de edad

--- src/Apps.js
+++ src/Apps.js
@@ -3,16 +3,30 @@ function get_nombre(nombre)
 {
   return nombre;
 }
-function get_genero(genero)
+function get_genero(genero , edad)
 {
-  if( genero=="Femenino" )
+  if(edad>=30)
   {
-    return "Sra";
+    if( genero=="Femenino" )
+    {
+      return "Sra";
+    }
+    else if(genero=="Masculino" )
+    {
+      return "Sr";
+    }
   }
-  else if(genero=="Masculino" )
-  {
-    return "Sr";
+  else{
+    if( genero=="Femenino" )
+    {
+      return "Srita";
+    }
+    else if(genero=="Masculino" )
+    {
+      return "Joven";
+    }
   }
+ 
 }
   export {get_nombre,get_genero};
   
--- src/presenter.js
+++ src/presenter.js
@@ -4,15 +4,17 @@ import { get_genero } from "./Apps";
 
 const nombre = document.querySelector("#nombre");
 const genero = document.querySelector("#genero");
+const edad = document.querySelector("#edad");
 const form = document.querySelector("#nombre-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const ed=Number.parseInt(edad.value);
   const nom=nombre.value;
   const gen=genero.value;
 
-  div.innerHTML = "<p>" + " hola "+ get_genero(gen) +" "+ get_nombre(nom) + "</p>";
+  div.innerHTML = "<p>" + " hola "+ get_genero(gen,ed) +" "+ get_nombre(nom) + "</p>";
 
 });
